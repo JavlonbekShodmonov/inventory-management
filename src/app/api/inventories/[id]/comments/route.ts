22d0d2fad3a76edd0e48e20643ce68340d9f79e8
Javlon <mohiiish.com@gmail.com>
2025-11-05 11:56:48 +0500

    com

--- src/app/api/inventories/[id]/comments/route.ts
+++ src/app/api/inventories/[id]/comments/route.ts
@@ -6,12 +6,13 @@ import { prisma } from "@/lib/prisma";
 // Get comments
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const {id} = await params;
     const comments = await prisma.comment.findMany({
       where: {
-        inventoryId: params.id,
+        inventoryId: id,
       },
       include: {
         user: {
